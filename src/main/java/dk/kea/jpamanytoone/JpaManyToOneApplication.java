cf2fd8e6926fa048733720960a3b030d8220a548
Teller501 <ande777j@stud.kea.dk>
2023-09-01 09:07:07 +0200

    initial commit

---
+++ src/main/java/dk/kea/jpamanytoone/JpaManyToOneApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.jpamanytoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManyToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaManyToOneApplication.class, args);
+    }
+
+}
