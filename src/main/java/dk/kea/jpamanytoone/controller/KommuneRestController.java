54643fb7aab37404c805607c6a15d16081a8b187
Teller501 <ande777j@stud.kea.dk>
2023-09-05 16:24:38 +0200

    added all crud operations

--- src/main/java/dk/kea/jpamanytoone/controller/KommuneRestController.java
+++ src/main/java/dk/kea/jpamanytoone/controller/KommuneRestController.java
@@ -6,10 +6,11 @@ import dk.kea.jpamanytoone.repository.KommuneRepository;
 import dk.kea.jpamanytoone.service.ApiServiceGetKommuner;
 import dk.kea.jpamanytoone.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class KommuneRestController {
@@ -30,5 +31,34 @@ public class KommuneRestController {
     public List<Kommune> getKommuner(){
         List<Kommune> lstKommuner = kommuneRepository.findAll();
         return lstKommuner;
+    }
+
+    @GetMapping("/kommuner/{id}")
+    public ResponseEntity<Kommune> getKommuneById(@PathVariable("id") String id){
+        Optional<Kommune> kommuneOptional = kommuneRepository.findById(id);
+        if (kommuneOptional.isPresent()){
+            Kommune kommune = kommuneOptional.get();
+            return ResponseEntity.ok(kommune);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/kommuner")
+    public ResponseEntity<Kommune> postRegion(@RequestBody Kommune kommune){
+        Kommune kommune1 = kommuneRepository.save(kommune);
+        return ResponseEntity.ok(kommune1);
+    }
+
+    @PutMapping("/kommuner/{id}")
+    public ResponseEntity<Kommune> putKommune(@PathVariable("id") String id, @RequestBody Kommune kommune){
+        Optional<Kommune> kommuneOptional = kommuneRepository.findById(id);
+        if (kommuneOptional.isPresent()){
+            kommune.setKode(id);
+            kommuneRepository.save(kommune);
+            return ResponseEntity.ok(kommune);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/dk/kea/jpamanytoone/controller/RegionRestController.java
+++ src/main/java/dk/kea/jpamanytoone/controller/RegionRestController.java
@@ -6,10 +6,7 @@ import dk.kea.jpamanytoone.repository.RegionRepository;
 import dk.kea.jpamanytoone.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +56,24 @@ public class RegionRestController {
         if (regionOptional.isPresent()){
             regionRepository.deleteById(id);
             return ResponseEntity.ok("Region slettet");
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/regioner")
+    public ResponseEntity<Region> postRegion(@RequestBody Region region){
+        Region region1 = regionRepository.save(region);
+        return ResponseEntity.ok(region1);
+    }
+
+    @PutMapping("/regioner/{id}")
+    public ResponseEntity<Region> putRegion(@PathVariable("id") String id, @RequestBody Region region){
+        Optional<Region> regionOptional = regionRepository.findById(id);
+        if (regionOptional.isPresent()){
+            region.setKode(id);
+            regionRepository.save(region);
+            return ResponseEntity.ok(region);
         } else{
             return ResponseEntity.notFound().build();
         }
