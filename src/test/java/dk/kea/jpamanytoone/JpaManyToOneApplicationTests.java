112f1665cadeaf84e1777d0ec9691121c7bddb4e
Teller501 <ande777j@stud.kea.dk>
2023-09-05 16:36:35 +0200

    fixed test

--- src/test/java/dk/kea/jpamanytoone/JpaManyToOneApplicationTests.java
+++ src/test/java/dk/kea/jpamanytoone/JpaManyToOneApplicationTests.java
@@ -3,7 +3,7 @@ package dk.kea.jpamanytoone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JpaManyToOneApplication.class)
 class JpaManyToOneApplicationTests {
 
     @Test
